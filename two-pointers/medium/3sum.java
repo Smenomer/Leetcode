cc9336b742d523d504460487e50b243e3d9357c2
Sujay Joshi <145902128+Smenomer@users.noreply.github.com>
2026-08-31 20:21:36 +0530

    leetcode 15: update solution

---
+++ two-pointers/medium/3sum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+       Arrays.sort(nums);
+       Set<List<Integer>> set=new HashSet<>();
+
+       for(int i=0;i<nums.length-2;i++){
+        int l=i+1;
+        int r=nums.length-1;
+        while(l<r){
+            if(nums[i]+nums[l]+nums[r]==0){
+                set.add(Arrays.asList(nums[i],nums[l],nums[r]));
+                l++;
+                r--;
+            }
+            if(nums[i]+nums[l]+nums[r]<0){
+                l++;
+            }
+            if(nums[i]+nums[l]+nums[r]>0){
+                r--;
+            }
+        }
+       } return new ArrayList<>(set);
+    }
+}

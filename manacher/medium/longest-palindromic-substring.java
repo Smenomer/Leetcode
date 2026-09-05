aaa7eebcab504e08edcb55256ecfcf7a7c5dcf7d
Sujay Joshi <145902128+Smenomer@users.noreply.github.com>
2026-09-05 23:42:36 +0530

    leetcode 5: update solution

---
+++ manacher/medium/longest-palindromic-substring.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String longestPalindrome(String s) {
+         int beststart=0;
+        int reslen=0;
+        for(int i=0;i<s.length();i++){
+            int l=i;
+            int r=i;
+          
+            while(l>=0 && r<s.length() && s.charAt(l)==s.charAt(r)){
+                if(r-l+1>reslen){
+                   beststart=l;
+                    reslen=r-l+1;
+                } l--;
+                r++;
+                
+            }
+            l=i;
+            r=i+1;
+            while(l>=0 && r<s.length() && s.charAt(l)==s.charAt(r)){
+                if(r-l+1>reslen){
+                   beststart=l;
+                    reslen=r-l+1;  
+                }l--;
+                r++;
+                   
+            }
+        } return s.substring(beststart,beststart+reslen);
+    }
+}
